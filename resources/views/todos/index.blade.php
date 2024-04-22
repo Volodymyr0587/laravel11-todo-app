4b960dc73092bcb11064d56c793709806a2cae60
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-04-22 13:50:52 +0300

    Show created and updated date

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -30,15 +30,22 @@
                         </div>
                         <div>
                             @forelse ($todos as $todo)
-                            <div class="flex mb-4 items-center">
-                                <p class="w-full text-grey-darkest">{{ $todo->description }}</p>
-                                <form action="{{ route('todos.complete', $todo) }}" method="POST">
-                                    @csrf
-                                    @method('PUT')
-                                    <button type="submit"
-                                        class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-blue-500">Done</button>
-                                </form>
-                                <a href="{{ route('todos.edit', $todo) }}" class="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-blue-500 hover:bg-teal">Edit</a>
+                            <div class="flex mb-4 items-center justify-between">
+                                <div>
+                                    <p class="w-full text-grey-darkest text-lg">{{ $todo->description }}</p>
+                                    <p class="mt-2 text-sm font-bold italic">Created at {{ $todo->created_at }}</p>
+                                    <p class="mt-2 text-sm font-bold italic">Updated at {{ $todo->updated_at }}</p>
+                                </div>
+                                <div class="flex justify-between">
+                                    <form action="{{ route('todos.complete', $todo) }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <button type="submit"
+                                            class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-blue-500">Done</button>
+                                    </form>
+                                    <a href="{{ route('todos.edit', $todo) }}" class="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-blue-500 hover:bg-teal">Edit</a>
+                                </div>
+
                             </div>
                             @empty
                             <div class="flex mb-4 items-center">No Todos Yet.</div>
