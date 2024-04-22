8582c95f5fe43fcaeb1d765665fd9fee8bd937d1
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-04-22 11:51:06 +0300

    show old vale in form after validation error

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -18,7 +18,7 @@
                                 @csrf
                                 <input type="text" name="description" id="description"
                                     class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
-                                    placeholder="Add Todo">
+                                    placeholder="Add Todo" value="{{ old('description') }}">
                                 <button type="submit"
                                     class="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-blue-500 hover:bg-teal">Add</button>
                             </form>
