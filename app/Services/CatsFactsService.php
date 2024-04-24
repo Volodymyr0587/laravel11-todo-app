6679fb96055ade22cbc02eb1229a3b5fb3851d96
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-04-24 20:01:15 +0300

    Cache result of api call to get random cats facts

--- app/Services/CatsFactsService.php
+++ app/Services/CatsFactsService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 
@@ -10,11 +11,22 @@ class CatsFactsService
 
     public function getRandomCatFact()
     {
+        $cacheKey = 'random_cat_fact';
+
+        if (Cache::has($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+
         try {
             $response = Http::get('https://meowfacts.herokuapp.com/?lang=ukr');
             $data =  collect(json_decode($response->body()));
 
-            return $data['data'][0];
+            $randomFact = $data['data'][0];
+
+            // Cache the data for 1 hour (you can adjust the expiration time as needed)
+            Cache::put($cacheKey, $randomFact, now()->addMinute());
+
+            return $randomFact;
         } catch (\Exception $e) {
             return null;
         }
