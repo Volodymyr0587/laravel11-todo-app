0036916b0535db37f70c08dac33232388fc8169e
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-04-27 21:18:47 +0300

    place buttons for edit form

--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -14,17 +14,22 @@
                     <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
                         <div class="mb-4">
 
-                            <form class="flex mt-4" action="{{ route('todos.update', $todo->id) }}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <input type="text" name="description" id="description" value="{{ $todo->description }}"
-                                    class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
-                                    placeholder="Add Todo">
-                                <button type="submit"
-                                    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-yellow-500 hover:bg-yellow-700 text-white shadow-md shadow-yellow-900/10 hover:shadow-lg hover:shadow-yellow-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">Update</button>
-                                    <a href="{{ route('todos.index') }}"
-                                    class=" ml-2 align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-blue-500 hover:bg-blue-700 text-white shadow-md shadow-blue-900/10 hover:shadow-lg hover:shadow-blue-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">Cancel</a>
-                            </form>
+                            <div class="flex flex-col">
+                                <form action="{{ route('todos.update', $todo->id) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <input type="text" name="description" id="description" value="{{ $todo->description }}"
+                                        class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                                        placeholder="Add Todo">
+                                    <div class="flex flex-col mt-2 sm:flex-row sm:justify-center">
+                                        <button type="submit"
+                                            class="w-full sm:w-auto mb-2 sm:mb-0 select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-yellow-500 hover:bg-yellow-700 text-white shadow-md shadow-yellow-900/10 hover:shadow-lg hover:shadow-yellow-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">Update</button>
+                                        <a href="{{ route('todos.index') }}"
+                                            class="w-full sm:w-auto ml-0 sm:ml-2 select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-blue-500 hover:bg-blue-700 text-white shadow-md shadow-blue-900/10 hover:shadow-lg hover:shadow-blue-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">Cancel</a>
+                                    </div>
+                                </form>
+                            </div>
+
 
                             @error('description')
                             <div class="mt-4 text-sm text-red-500">{{ $message }}</div>
