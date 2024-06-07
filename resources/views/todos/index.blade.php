5c9e8857f7b29965602bc18555041bac7e3c3373
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-06-07 11:08:09 +0300

    resize textarea

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -17,17 +17,17 @@
                 <!-- Section: Design Block -->
               <section class="mb-32">
                 <div class="flex justify-center">
-                  <div class="max-w-[700px] text-center">
+                  <div class="w-full text-center">
 
                     <div class="flex flex-col">
                         <form action="{{ route('todos.store') }}" method="POST">
                             @csrf
-                            <div class="flex flex-col mt-2 sm:flex-col sm:justify-center">
-                                <textarea type="text" name="description" id="description"
-                                    class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 mb-2 text-grey-darker"
+                            <div class="flex flex-col items-center mt-2 sm:flex-col sm:justify-center">
+                                <textarea type="text" name="description" id="description" rows="4"
+                                    class="shadow appearance-none border rounded w-full py-2 px-3 mb-2 text-grey-darker"
                                     placeholder="Add Todo" >{{ old('description') }}</textarea>
                                 <button type="submit"
-                                    class="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-yellow-500 hover:bg-yellow-700 text-white shadow-md shadow-yellow-900/10 hover:shadow-lg hover:shadow-yellow-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">Add</button>
+                                    class="w-1/3 align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-yellow-500 hover:bg-yellow-700 text-white shadow-md shadow-yellow-900/10 hover:shadow-lg hover:shadow-yellow-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none">Add</button>
                             </div>
                         </form>
                     </div>
